12dd47a770fb1cda7a28474527977baeaa4d6c9a
Sean Maxwell <s.a.maxwell@avanade.com>
2022-10-19 20:21:29 +0100

    Lecutre material

--- file.py
+++ file.py
@@ -1,1 +1,63 @@
-print("hello")
+########################Lecture Material covered##################################
+
+
+#print("Hello World")
+
+#Print the next word on the next line
+#print("Hello \nWorld")
+
+#var1 = 7
+#print(var1)
+
+# Converting 7 from integer to string gives the same output.
+#var1 = str(var1)
+#print(var1)
+
+
+#var1 = int(input("Enter a number : "))
+
+#Only turn something itno an integer if you know it can be turned into an integer
+#var2 = int(var1) * 2
+#print(var2)
+
+#Creating a float because you have a decimal point
+##var1 = 7.14
+
+#var2= int(var1)
+#var3= float(var1)
+
+#print(var3)
+
+#var1 = True
+#var2 = False
+#var3 = 3
+#var4 = "four"
+
+#var3 = bool(var3)
+
+
+#print(type(var1))
+# When you convert an boolean value into an integer everything besides the number 0 will return True.
+#print(type(var3))
+#print(var4)
+#print(type(var4))
+
+# var1 = input("Please enter your name : ")
+
+#########################Tutorial and exercise QA Academy######################
+
+english = 50
+maths = 60
+science = 70
+
+total = english + maths + science
+average = total / 3
+
+print("Your average score is : ", average)
+
+#Inputting scores
+english = input()
+maths = input()
+science = input()
+
+total = english + maths + science
