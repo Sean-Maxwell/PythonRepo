6b099733ba4ef494cec2b13f1ef1c738744d19ab
Sean Maxwell <s.a.maxwell@avanade.com>
2022-10-20 10:58:36 +0100

    adding files

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+name = input("Please enter your name : ")
+
+if name == "Dave":
+    print("Not today")
+elif name =="John":
+    print("We love John!")
+elif name =="Sally":
+    print("Wow its Sally!")
+else:
+    print(f"Hello {name}!")
+
+# Ctrl forward slash to comment a line automatically
+#num = int(input("Please enter a number: "))
+
+#if num > 5:
+#   print("Greater than 5")
+#if num > 10:
+#   print("Greater than 10")
---
+++ conditionals1.py
@@ -0,0 +1,11 @@
+mark = int(input("Inputs your number here : "))
+
+if mark >= 80:
+    print("Distinction")
+elif mark >= 70:
+    print("Merit")
+elif mark >= 60:
+    print("Pass")
+else:
+    print("Fail")
+   
